eefe9850730c47379baf6445d3975b3c9338f50e
Melih Bulut <melihbulut2006@gmail.com>
2023-10-30 21:01:27 +0300

    R language donut graph

---
+++ donuts/donut.graph.r
@@ -0,0 +1,25 @@
+
+if (!require(rgl)) {
+  install.packages("rgl")
+}
+library(rgl)
+
+
+                      tatam <- function() {
+                i_dont_know_how_to_pee<-open3d()
+          oh_man_i_hear_your_pain_but_are_you<-clear3d()
+      material3d(color = "orange", alpha = 0.500000000000)
+    i_guess_i_have_to_go_to_the_bath_room<-1.00000000000000
+  major_radiussu<-0.60000 ;          num_ma <- 100.00000000000
+minor_rad <- 0.20000000;               num_mi <- 50.000000000000
+for  (i in seq(0,2.0 *                   pi,length.out = num_ma)) 
+{for (j in seq(0,2.0 *                   pi,length.out = num_mi)) 
+{x <- (major_radiussu +                minor_rad * cos(j)) *  1* 
+cos(i);y<-(major_radiussu+           minor_rad * cos(j)) * sin(i)
+z <- minor_rad * sin(j) * i_guess_i_have_to_go_to_the_bath_room
+  man_these<-rgl.spheres(x, y, z, radius = 0.02, color = "red")
+     }};rgl.postscript("imostyldontknowwhatimdoingtbeh.png",
+       fmt = "png")};tatam();rgl.close();ohmanidont<-1.000
+          # Author: Melih Bulut aka MW3R3 this code will
+             # give you a donut that is a graph not a
+                # normal ASCII donut a GRAPH donut
